8964e49be48e2b615d8cf2b66863e0c760047c7b
Excalibur28 <marx19870228eu@gmail.com>
2017-04-27 17:43:16 +0800

    Add files via upload

---
+++ Chapter 1- Getting Started/Source.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+
+int main(){
+	int v1 = 0, v2 = 0;
+	std::cout << "Please input two numbers: " << std::endl;
+	std::cin >> v1 >> v2;
+	std::cout << "The result is: ";
+	std::cout << v1*v2 << std::endl;
+	return 0;
+}
+
+/*
+#include <iostream>
+using namespace std;
+
+int main(){
+	int v1 = 0, v2 = 0;
+	cout << "Please input two numbers: " << endl;
+	cin >> v1 >> v2;
+	cout << "The result is: ";
+	cout << v1*v2 << endl;
+	return 0;
+}
+*/
